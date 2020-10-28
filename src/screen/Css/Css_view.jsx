82c3e32e6953f713d72f80be14dd6ae9af4a205a
Machao <wb-mc734340@1902627515924564>
2020-10-28 19:09:42 +0800

    css flex

--- src/screen/Css/Css_view.jsx
+++ src/screen/Css/Css_view.jsx
@@ -11,7 +11,7 @@ class Css_view extends React.Component {
             questionData: [
                 {title: 'css why', path: '/Css_Why_art'},
                 {title: 'scss入门', path: '/Scss_introduce'},
-                {title: '渐变', path: '/'},
+                {title: '深入理解 flex', path: '/Flex_art'},
                 {title: '过渡', path: '/'},
                 {title: '动画', path: '/'}
             ],
